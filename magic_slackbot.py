5fb566daa38791288793c29b98b7763c13392a53
EC2 Default User <ec2-user@ip-172-31-38-222.us-east-2.compute.internal>
2021-10-04 22:54:42 +0000

    trying to deploy using app runner in AWS

--- magic_slackbot.py
+++ magic_slackbot.py
@@ -9,14 +9,14 @@ from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-# loads environment variables from a .env file 
+# loads environment variables from the .env file 
 
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
 # need to set up the slack events to use Flask web server (where we send the events to)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-client.chat_postMessage(channel='#test', text='Hello friends!')
+client.chat_postMessage(channel='#test', text='Hello amigos!')
 
 
 if __name__ == "__main__":
