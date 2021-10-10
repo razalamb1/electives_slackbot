c81e9f07754a16765a0f98aa78614f6664499de1
EC2 Default User <ec2-user@ip-172-31-38-222.us-east-2.compute.internal>
2021-10-10 18:43:41 +0000

    im scared my environment variables are going to check in again

--- magic_slackbot.py
+++ magic_slackbot.py
@@ -9,17 +9,17 @@ from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-# loads environment variables from the .env file 
 
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
 # need to set up the slack events to use Flask web server (where we send the events to)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-client.chat_postMessage(channel='#test', text='Hello amigos!')
 
+# loads environment variables from the .env file
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client.chat_postMessage(channel='#general', text='Hola! Yes, I am alive here too')
 
 if __name__ == "__main__":
     app.run(debug=True)
-    #will run in default port, 5000?
+    # will this thing run in default port, 5000?
     # if I save this file, modify it, I dont need to run the python script again. It will automatically update the web server
