eb2d9373c4a1db7d9fbb4fd6bff8ad17f229904e
EC2 Default User <ec2-user@ip-172-31-38-222.us-east-2.compute.internal>
2021-10-12 01:32:38 +0000

    changed order

--- magic_slackbot.py
+++ magic_slackbot.py
@@ -7,12 +7,12 @@ from dotenv import load_dotenv
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 
+# loads environment variables from the .env file
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
-# need to set up the slack events to use Flask web server (where we send the events to)
+
 
 @app.route('/')
 def hello():
@@ -20,7 +20,12 @@ def hello():
     print("I am inside hello world")
     return 'Hello World! I can make change at route: /change'
 
-# loads environment variables from the .env file
+
+# need to set up the slack events to use Flask web server (where we send the events to)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
+
+
+
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 client.chat_postMessage(channel='#general', text='Hola! Yes, I am alive here too')
 
@@ -28,4 +33,4 @@ client.chat_postMessage(channel='#general', text='Hola! Yes, I am alive here too
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
     # will this thing run in default port, 5000?
-    # if I save this file, modify it, I dont need to run the python script again. It will automatically update the web server
+    # "debug" : if I save this file, modify it, I dont need to run the python script again. It will automatically update the web server
